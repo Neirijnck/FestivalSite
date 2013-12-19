3f72fbd57d36aff17d1ea68c1db79466284d9cf0
Neirijnck <neirijnck@hotmail.com>
2013-12-19 17:40:45 +0100

    FestivalSite V0.05
    -Kan foto's tonen van bands op Home samen met nieuws
    -Responsive slider toegevoegd met bandfoto's op homepagina

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -18,10 +18,11 @@ namespace FestivalSite
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/readmore").Include(
-                        "~/Scripts/readmore.js"
-                ));
+            bundles.Add(new ScriptBundle("~/bundles/responsiveSlides").Include(
+                        "~/Scripts/responsiveslides.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/expander").Include("~/Scripts/jquery.expander.min.js"));
+            
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
